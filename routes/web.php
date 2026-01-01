feb38bd6db36652124916a0bee1ae4f0fffb96ae
AbdAlhadi Jouda <abdalhadijouda@gmail.com>
2026-01-01 17:02:15 +0200

    Add route name for login endpoint in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthenticationController::class, 'login'])
+    ->name('login')
     ->middleware('guest');
 
 Route::post('/logout', [AuthenticationController::class, 'logout'])
