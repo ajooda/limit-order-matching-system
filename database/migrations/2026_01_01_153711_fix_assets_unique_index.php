23e875dc3db63555bd08aab8ac8cfb97a1a7ae27
AbdAlhadi Jouda <abdalhadijouda@gmail.com>
2026-01-01 17:39:14 +0200

    Fix unique index on 'symbol' column in assets table

---
+++ database/migrations/2026_01_01_153711_fix_assets_unique_index.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('assets', function (Blueprint $table) {
+            $table->dropUnique('assets_symbol_unique');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $table->unique('symbol');
+    }
+};
