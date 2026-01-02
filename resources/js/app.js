9486948bf44f0fb723917efafc9de3ada6755dc9
AbdAlhadi Jouda <abdalhadijouda@gmail.com>
2026-01-02 18:59:15 +0200

    Integrate Pinia for state management

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,22 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import router from './router';
-import App from './App.vue';
+import './bootstrap'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router'
+import App from './App.vue'
+import { useAuthStore } from './stores/auth.js'
+import { initializeEcho } from './echo.js'
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
+app.use(router)
+
+const authStore = useAuthStore()
+
+authStore.bootstrap().then((isAuthenticated) => {
+    if (isAuthenticated) {
+        initializeEcho()
+    }
+    app.mount('#app')
+})
